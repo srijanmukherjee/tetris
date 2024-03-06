dd380bc10aae0721e06bc0d5e008b7206440d82b
Srijan Mukherjee <emailofsrijan@gmail.com>
2024-03-06 22:17:23 +0530

    added home screen

--- Makefile
+++ Makefile
@@ -1,19 +1,22 @@
 CC=gcc
-CFLAGS=-Wall -ggdb `sdl2-config --cflags` `pkg-config --cflags SDL2_image SDL2_mixer`
-LIBS=`sdl2-config --libs` `pkg-config --libs SDL2_image SDL2_mixer`
+CFLAGS=-Wall -ggdb `sdl2-config --cflags` `pkg-config --cflags SDL2_image SDL2_mixer SDL2_ttf` -lm
+LIBS=`sdl2-config --libs` `pkg-config --libs SDL2_image SDL2_mixer SDL2_ttf`
 BUILDIR=build
 SRCDIR=src
 EXECUTABLE=tetris
 
-all: $(BUILDIR)/$(EXECUTABLE)
+all: $(BUILDIR)/$(EXECUTABLE) assets
 
-assets: assets/img/pallete.png
+assets: assets/img/pallete.png assets/img/play_btn.png
 
 $(BUILDIR)/$(EXECUTABLE): $(SRCDIR)/main.c
 	$(CC) $(CFLAGS) -o $(BUILDIR)/$(EXECUTABLE) $(SRCDIR)/main.c $(LIBS)
 
 assets/img/pallete.png: assets/img/pallete.svg
 	convert -background none assets/img/pallete.svg assets/img/pallete.png
+
+assets/img/play_btn.png: assets/img/play_btn.svg
+	convert -background none assets/img/play_btn.svg assets/img/play_btn.png
 
 clean:
 	rm build/*
--- src/main.c
+++ src/main.c
@@ -5,13 +5,16 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_mouse.h>
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_stdinc.h>
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_timer.h>
+#include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_video.h>
 #include <errno.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -26,7 +29,12 @@
 #define BOARD_WIDTH 10
 #define BOARD_HEIGHT 20
 
-const char tetriminos[7][16] = {"..x...x...x...x.", ".....xx..xx.....",
+#define SCREEN_EXIT -1
+#define SCREEN_HOME 0
+#define SCREEN_PLAY 1
+#define SCREEN_GAME_OVER 2
+
+const char tetriminos[7][16] = {"..x...x...x...x.", ".xx..xx.........",
                                 ".....xx.xx......", "....xx...xx.....",
                                 ".....x...x...xx.", "......x...x..xx.",
                                 "....xxx..x......"};
@@ -42,6 +50,22 @@ typedef struct {
     int y;
     int rotation;
 } Piece;
+
+typedef struct {
+    Piece piece;
+    bool over;
+    float y;
+    int yspeed;
+    uint32_t board_xoff;
+    uint32_t board_yoff;
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Texture *pieces_texture;
+    SDL_Texture *play_btn_texture;
+    Mix_Music *bg_music;
+    TTF_Font *font;
+    SDL_Texture *home_screen_header;
+} Game;
 
 // returns the index of (x, y) corresponding to the rotated shape
 int rotate(int x, int y, int rotation) {
@@ -100,6 +124,231 @@ float get_delta() {
     return dt;
 }
 
+int home_screen(Game *game) {
+    static int y = 0;
+    static double counter = 0;
+
+    int play_btn_width = 40;
+    int play_btn_height = 40;
+    int px = WINDOW_WIDTH / 2 - play_btn_width / 2;
+    int py = WINDOW_HEIGHT / 2 + 100 - play_btn_height / 2;
+    bool clicked = false;
+
+    SDL_Event e;
+    if (SDL_PollEvent(&e)) {
+        if (e.type == SDL_QUIT) {
+            return SCREEN_EXIT;
+        }
+
+        // detect mouse click
+        if (e.type == SDL_MOUSEBUTTONUP) {
+            clicked = true;
+        }
+    }
+
+    int mx;
+    int my;
+    SDL_GetMouseState(&mx, &my);
+
+    if (mx >= px && mx <= px + play_btn_width && my >= py &&
+        my <= py + play_btn_height) {
+        if (clicked)
+            return SCREEN_PLAY;
+        play_btn_width *= 2;
+        play_btn_height *= 2;
+        px = WINDOW_WIDTH / 2 - play_btn_width / 2;
+        py = WINDOW_HEIGHT / 2 + 100 - play_btn_height / 2;
+    }
+
+    SDL_SetRenderDrawColor(game->renderer, 51, 51, 51, 255);
+    SDL_RenderClear(game->renderer);
+
+    float dt = get_delta();
+    counter = (counter + 5 * dt);
+    if (counter > INT_MAX)
+        counter = 0;
+    y = 10 * sin(counter);
+
+    SDL_RenderCopy(game->renderer, game->home_screen_header, NULL,
+                   &(SDL_Rect){WINDOW_WIDTH / 2 - 150,
+                               WINDOW_HEIGHT / 2 - 75 - 100 + y, 300, 150});
+
+    SDL_RenderCopy(game->renderer, game->play_btn_texture, NULL,
+                   &(SDL_Rect){px, py, play_btn_width, play_btn_height});
+
+    SDL_RenderPresent(game->renderer);
+    return SCREEN_HOME;
+}
+
+int play_screen(Game *game) {
+    float dt = get_delta();
+    SDL_Event e;
+    if (SDL_PollEvent(&e)) {
+        if (e.type == SDL_QUIT) {
+            return SCREEN_EXIT;
+        }
+
+        if (e.type == SDL_KEYDOWN) {
+            switch (e.key.keysym.sym) {
+            case SDLK_w:
+            case SDLK_UP: {
+                int nextRot = (game->piece.rotation + 1) % 4;
+                if (does_piece_fit(game->piece.shape, game->piece.x,
+                                   game->piece.y, nextRot)) {
+                    game->piece.rotation = nextRot;
+                }
+                break;
+            }
+            case SDLK_d:
+            case SDLK_RIGHT:
+                if (does_piece_fit(game->piece.shape, game->piece.x + 1,
+                                   game->piece.y, game->piece.rotation))
+                    game->piece.x += 1;
+                break;
+            case SDLK_a:
+            case SDLK_LEFT:
+                if (does_piece_fit(game->piece.shape, game->piece.x - 1,
+                                   game->piece.y, game->piece.rotation))
+                    game->piece.x -= 1;
+                break;
+            case SDLK_s:
+            case SDLK_DOWN:
+                game->y += 1;
+                break;
+            }
+        }
+    }
+
+    if (game->over)
+        return SCREEN_PLAY;
+
+    SDL_SetRenderDrawColor(game->renderer, 51, 51, 51, 255);
+    SDL_RenderClear(game->renderer);
+
+    // update
+    if (does_piece_fit(game->piece.shape, game->piece.x, game->piece.y + 1,
+                       game->piece.rotation)) {
+        game->y += dt * game->yspeed;
+        game->piece.y = game->y;
+    } else {
+        // piece can't fit, so copy it onto the board
+        for (int px = 0; px < 4; px++) {
+            for (int py = 0; py < 4; py++) {
+                if (game->piece.shape[rotate(px, py, game->piece.rotation)] ==
+                    'x') {
+                    board[(game->piece.y + py) * BOARD_WIDTH +
+                          (game->piece.x + px)] = game->piece.color + 1;
+                }
+            }
+        }
+
+        // check if line is formed
+        for (int py = 0; py < 4; py++) {
+            if (game->piece.y + py >= BOARD_HEIGHT)
+                break;
+            bool line = true;
+
+            for (int px = 0; px < BOARD_WIDTH; px++) {
+                line &= board[(game->piece.y + py) * BOARD_WIDTH + px] != 0;
+            }
+
+            if (line) {
+                for (int px = 0; px < BOARD_WIDTH; px++) {
+                    board[(game->piece.y + py) * BOARD_WIDTH + px] = 127;
+                }
+
+                lines[line_ptr++] = game->piece.y + py;
+            }
+        }
+
+        int shape = rand() % 7;
+        game->piece.shape = tetriminos[shape];
+        game->y = 0;
+        game->piece.y = 0;
+        game->piece.color = shape;
+        game->piece.x = BOARD_WIDTH / 2;
+        game->piece.rotation = 0;
+
+        game->over = !does_piece_fit(game->piece.shape, game->piece.x,
+                                     game->piece.y, game->piece.rotation);
+
+        if (game->over) {
+            // TODO : draw only overlapping portion
+            return SCREEN_PLAY;
+        }
+    }
+
+    // draw board
+    for (int x = 0; x < BOARD_WIDTH; x++) {
+        for (int y = 0; y < BOARD_HEIGHT; y++) {
+            int val = board[y * BOARD_WIDTH + x];
+            if (val == 0) {
+                SDL_SetRenderDrawColor(game->renderer, 28, 28, 28, 255);
+                SDL_RenderFillRect(
+                    game->renderer,
+                    &(SDL_Rect){game->board_xoff + x * PIECE_WIDTH,
+                                game->board_yoff + y * PIECE_HEIGHT,
+                                PIECE_WIDTH, PIECE_HEIGHT});
+            } else if (val == 127) {
+                SDL_SetRenderDrawColor(game->renderer, 255, 255, 255, 255);
+                SDL_RenderFillRect(
+                    game->renderer,
+                    &(SDL_Rect){game->board_xoff + x * PIECE_WIDTH,
+                                game->board_yoff + y * PIECE_HEIGHT,
+                                PIECE_WIDTH, PIECE_HEIGHT});
+            } else {
+                SDL_RenderCopy(
+                    game->renderer, game->pieces_texture,
+                    &(SDL_Rect){(val - 1) * 30, 0, 30, 30},
+                    &(SDL_Rect){
+                        game->board_xoff + x * PIECE_WIDTH + PIECE_PADDING,
+                        game->board_yoff + y * PIECE_HEIGHT + PIECE_PADDING,
+                        PIECE_WIDTH - 2 * PIECE_PADDING,
+                        PIECE_HEIGHT - 2 * PIECE_PADDING});
+            }
+        }
+    }
+
+    // draw current piece
+    for (int px = 0; px < 4; px++) {
+        for (int py = 0; py < 4; py++) {
+            int idx = rotate(px, py, game->piece.rotation);
+            if (game->piece.shape[idx] == 'x') {
+                SDL_RenderCopy(
+                    game->renderer, game->pieces_texture,
+                    &(SDL_Rect){game->piece.color * 30, 0, 30, 30},
+                    &(SDL_Rect){
+                        game->board_xoff + (game->piece.x + px) * PIECE_WIDTH +
+                            PIECE_PADDING,
+                        game->board_yoff + (game->piece.y + py) * PIECE_HEIGHT,
+                        PIECE_WIDTH - 2 * PIECE_PADDING,
+                        PIECE_HEIGHT - 2 * PIECE_PADDING});
+            }
+        }
+    }
+
+    // animate line completion
+    if (line_ptr > 0) {
+        SDL_RenderPresent(game->renderer);
+        SDL_Delay(100);
+        for (int i = 0; i < line_ptr; i++) {
+            for (int px = 0; px < BOARD_WIDTH; px++) {
+                for (int py = lines[i]; py > 0; py--)
+                    board[py * BOARD_WIDTH + px] =
+                        board[(py - 1) * BOARD_WIDTH + px];
+                board[px] = 0;
+            }
+        }
+        line_ptr = 0;
+        get_delta();
+    }
+
+    SDL_RenderPresent(game->renderer);
+    return SCREEN_PLAY;
+}
+
+int game_over_screen(Game *game) { return 2; }
+
 int main() {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_AUDIO) < 0) {
         fprintf(stderr, "ERROR: failed to initialize SDL: %s\n",
@@ -109,6 +358,12 @@ int main() {
 
     if (IMG_Init(IMG_INIT_PNG) == 0) {
         fprintf(stderr, "ERROR: failed to initialize SDL Image: %s\n",
+                SDL_GetError());
+        exit(1);
+    }
+
+    if (TTF_Init() < 0) {
+        fprintf(stderr, "ERROR: failed to initialize ttf library: %s\n",
                 SDL_GetError());
         exit(1);
     }
@@ -148,190 +403,67 @@ int main() {
         exit(1);
     }
 
-    Mix_VolumeMusic(50);
-
-    Mix_Music *bg_music = Mix_LoadMUS("./assets/music/bg.mp3");
-
-    Mix_FadeInMusic(bg_music, -1, 100);
+    Mix_VolumeMusic(10);
 
     SDL_Surface *color_pieces_surface = IMG_Load("./assets/img/pallete.png");
     SDL_Texture *color_pieces_texture =
         SDL_CreateTextureFromSurface(renderer, color_pieces_surface);
 
-    Piece piece = {.shape = tetriminos[4], .color = 4, .x = 0, .y = 0};
-    float y = 0;
-    int yspeed = 5;
-    int board_xoff = (WINDOW_WIDTH - BOARD_WIDTH * PIECE_WIDTH) / 2;
-    int board_yoff = (WINDOW_HEIGHT - BOARD_HEIGHT * PIECE_HEIGHT) / 2;
+    SDL_Surface *play_btn_surface = IMG_Load("./assets/img/play_btn.png");
+    SDL_Texture *play_btn_texture =
+        SDL_CreateTextureFromSurface(renderer, play_btn_surface);
+    SDL_FreeSurface(play_btn_surface);
 
-    bool game_over = false;
+    Game game = {.piece = {.shape = tetriminos[4],
+                           .color = 4,
+                           .x = BOARD_WIDTH / 2,
+                           .y = 0},
+                 .board_xoff = (WINDOW_WIDTH - BOARD_WIDTH * PIECE_WIDTH) / 2,
+                 .board_yoff =
+                     (WINDOW_HEIGHT - BOARD_HEIGHT * PIECE_HEIGHT) / 2,
+                 .over = false,
+                 .pieces_texture = color_pieces_texture,
+                 .play_btn_texture = play_btn_texture,
+                 .renderer = renderer,
+                 .window = window,
+                 .y = 0,
+                 .yspeed = 3};
+
+    game.bg_music = Mix_LoadMUS("./assets/music/bg.mp3");
+
+    Mix_FadeInMusic(game.bg_music, -1, 100);
+
+    // load font
+    game.font = TTF_OpenFont("./assets/font/SuperFunky.ttf", 80);
+    if (game.font == NULL) {
+        fprintf(stderr, "ERROR: failed to load font: %s\n", SDL_GetError());
+        exit(1);
+    }
+    SDL_Surface *surface = TTF_RenderText_Solid(
+        game.font, "The Tetris", (SDL_Color){255, 255, 255, 255});
+    game.home_screen_header = SDL_CreateTextureFromSurface(renderer, surface);
+    // SDL_FreeSurface(surface);
+
+    int screen = SCREEN_HOME;
 
     while (true) {
-        float dt = get_delta();
-        SDL_Event e;
-        if (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
-                break;
-            }
-
-            if (e.type == SDL_MOUSEBUTTONUP) {
-                piece.shape = tetriminos[rand() % 7];
-                piece.color = rand() % 7;
-            }
-
-            if (e.type == SDL_KEYDOWN) {
-                switch (e.key.keysym.sym) {
-                case SDLK_w:
-                case SDLK_UP: {
-                    int nextRot = (piece.rotation + 1) % 4;
-                    if (does_piece_fit(piece.shape, piece.x, piece.y,
-                                       nextRot)) {
-                        piece.rotation = nextRot;
-                    }
-                    break;
-                }
-                case SDLK_d:
-                case SDLK_RIGHT:
-                    if (does_piece_fit(piece.shape, piece.x + 1, piece.y,
-                                       piece.rotation))
-                        piece.x += 1;
-                    break;
-                case SDLK_a:
-                case SDLK_LEFT:
-                    if (does_piece_fit(piece.shape, piece.x - 1, piece.y,
-                                       piece.rotation))
-                        piece.x -= 1;
-                    break;
-                case SDLK_s:
-                case SDLK_DOWN:
-                    y += 1;
-                    break;
-                }
-            }
+        switch (screen) {
+        case SCREEN_HOME:
+            screen = home_screen(&game);
+            break;
+        case SCREEN_PLAY:
+            screen = play_screen(&game);
+            break;
+        case SCREEN_GAME_OVER:
+            screen = game_over_screen(&game);
+            break;
         }
 
-        if (game_over)
-            continue;
-
-        SDL_SetRenderDrawColor(renderer, 51, 51, 51, 255);
-        SDL_RenderClear(renderer);
-
-        // update
-        if (does_piece_fit(piece.shape, piece.x, piece.y + 1, piece.rotation)) {
-            y += dt * yspeed;
-            piece.y = y;
-        } else {
-            // piece can't fit, so copy it onto the board
-            for (int px = 0; px < 4; px++) {
-                for (int py = 0; py < 4; py++) {
-                    if (piece.shape[rotate(px, py, piece.rotation)] == 'x') {
-                        board[(piece.y + py) * BOARD_WIDTH + (piece.x + px)] =
-                            piece.color + 1;
-                    }
-                }
-            }
-
-            // check if line is formed
-            for (int py = 0; py < 4; py++) {
-                if (piece.y + py >= BOARD_HEIGHT)
-                    break;
-                bool line = true;
-
-                for (int px = 0; px < BOARD_WIDTH; px++) {
-                    line &= board[(piece.y + py) * BOARD_WIDTH + px] != 0;
-                }
-
-                if (line) {
-                    for (int px = 0; px < BOARD_WIDTH; px++) {
-                        board[(piece.y + py) * BOARD_WIDTH + px] = 127;
-                    }
-
-                    lines[line_ptr++] = piece.y + py;
-                }
-            }
-
-            int shape = rand() % 7;
-            piece.shape = tetriminos[shape];
-            y = 0;
-            piece.y = 0;
-            piece.color = shape;
-            piece.x = BOARD_WIDTH / 2;
-            piece.rotation = 0;
-
-            game_over =
-                !does_piece_fit(piece.shape, piece.x, piece.y, piece.rotation);
-
-            if (game_over) {
-                // TODO : draw only overlapping portion
-                break;
-            }
+        if (screen == SCREEN_EXIT) {
+            break;
         }
-
-        // draw board
-        for (int x = 0; x < BOARD_WIDTH; x++) {
-            for (int y = 0; y < BOARD_HEIGHT; y++) {
-                int val = board[y * BOARD_WIDTH + x];
-                if (val == 0) {
-                    SDL_SetRenderDrawColor(renderer, 28, 28, 28, 255);
-                    SDL_RenderFillRect(
-                        renderer, &(SDL_Rect){board_xoff + x * PIECE_WIDTH,
-                                              board_yoff + y * PIECE_HEIGHT,
-                                              PIECE_WIDTH, PIECE_HEIGHT});
-                } else if (val == 127) {
-                    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-                    SDL_RenderFillRect(
-                        renderer, &(SDL_Rect){board_xoff + x * PIECE_WIDTH,
-                                              board_yoff + y * PIECE_HEIGHT,
-                                              PIECE_WIDTH, PIECE_HEIGHT});
-                } else {
-                    SDL_RenderCopy(
-                        renderer, color_pieces_texture,
-                        &(SDL_Rect){(val - 1) * 30, 0, 30, 30},
-                        &(SDL_Rect){
-                            board_xoff + x * PIECE_WIDTH + PIECE_PADDING,
-                            board_yoff + y * PIECE_HEIGHT + PIECE_PADDING,
-                            PIECE_WIDTH - 2 * PIECE_PADDING,
-                            PIECE_HEIGHT - 2 * PIECE_PADDING});
-                }
-            }
-        }
-
-        // draw current piece
-        for (int px = 0; px < 4; px++) {
-            for (int py = 0; py < 4; py++) {
-                int idx = rotate(px, py, piece.rotation);
-                if (piece.shape[idx] == 'x') {
-                    SDL_RenderCopy(
-                        renderer, color_pieces_texture,
-                        &(SDL_Rect){piece.color * 30, 0, 30, 30},
-                        &(SDL_Rect){board_xoff + (piece.x + px) * PIECE_WIDTH +
-                                        PIECE_PADDING,
-                                    board_yoff + (piece.y + py) * PIECE_HEIGHT,
-                                    PIECE_WIDTH - 2 * PIECE_PADDING,
-                                    PIECE_HEIGHT - 2 * PIECE_PADDING});
-                }
-            }
-        }
-
-        // animate line completion
-        if (line_ptr > 0) {
-            SDL_RenderPresent(renderer);
-            SDL_Delay(100);
-            for (int i = 0; i < line_ptr; i++) {
-                for (int px = 0; px < BOARD_WIDTH; px++) {
-                    for (int py = lines[i]; py > 0; py--)
-                        board[py * BOARD_WIDTH + px] =
-                            board[(py - 1) * BOARD_WIDTH + px];
-                    board[px] = 0;
-                }
-            }
-            line_ptr = 0;
-            get_delta();
-        }
-
-        SDL_RenderPresent(renderer);
     }
-
+    SDL_DestroyTexture(game.home_screen_header);
     SDL_DestroyTexture(color_pieces_texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
